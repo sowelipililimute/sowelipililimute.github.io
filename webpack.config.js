5c90181c4d3704b65def05c458e12ecb63b4ba4b
Carson Black <uhhadd@gmail.com>
2020-04-05 15:36:30 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: ['./src/scss/app.scss', './src/js/app.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              },
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ],
+  },
+  devServer: {
+    port: 8000,
+  }
+};
